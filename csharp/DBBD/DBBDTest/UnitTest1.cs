473157ef1977a607ef3a814c40b74c7925410d4d
DESKTOP-E8S5FMF\백종환 <bsrk1013@naver.com>
2021-03-02 17:07:46 +0900

    공부

--- csharp/DBBD/DBBDTest/UnitTest1.cs
+++ csharp/DBBD/DBBDTest/UnitTest1.cs
@@ -13,8 +13,18 @@ namespace DBBDTest
         [Test]
         public void BufferTest()
         {
-
             Buffer buffer = new Buffer(256);
+
+            int a1 = 10;
+
+            //void write<T>(Buffer buffer, T value)
+            //{
+            //    uint size = (uint)System.Runtime.InteropServices.Marshal.SizeOf(value);
+            //    for(uint i = 0; i < size; i++)
+            //    {
+            //        char data = (char)(value >> i * 8);
+            //    }
+            //}
         }
 
         [Test]
